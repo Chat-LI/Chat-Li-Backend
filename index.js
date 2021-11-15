19d4e9f8eecb768d1a41bc006010e60b230e5fe1
kason <kbraley9@gmail.com>
2021-11-15 13:21:02 -0700

    (socket): Add message event listener

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const io = new Server();
 
 io.on('connection', (socket) => {
   console.log(`Socket ${socket.id} connected`);
+  socket.on('message', (payload) => {
+    io.emit('message', payload);
+  });
 });
 
 io.listen(3000);
