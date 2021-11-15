0340938076807509a10f85798441e700326d8251
kason <kbraley9@gmail.com>
2021-11-15 15:39:41 -0700

    Integrate basic socket setup into src/server.js

--- src/server.js
+++ src/server.js
@@ -21,6 +21,17 @@ app.use('/users', userRoutes);
 
 io.on('connection', (socket) => {
   console.log(`Socket ${socket.id} connected`);
+
+  socket.on('message', (payload) => {
+    io.emit('message', payload);
+  });
+
+  socket.on('join', (room) => {
+    console.log('Yes! I just joined the room!', room);
+    socket.join(room);
+  });
+
+  io.to('General Room 1').emit('Something');
 });
 
 module.exports = {
