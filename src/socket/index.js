8edd4e595669468f3b1bc5a5584b14b831509a6f
kason <kbraley9@gmail.com>
2021-11-16 16:40:52 -0700

    (feat): Add /listUsers and /listRoomUsers cmds

--- src/socket/index.js
+++ src/socket/index.js
@@ -16,5 +16,41 @@ module.exports = function (io) {
       socket.broadcast.emit('quit', socket.id);
       socket.disconnect(true);
     });
+
+    socket.on('listUsers', () => {
+      let allUsers = [];
+
+      for (let [key, value] of io.of('/').sockets) {
+        console.log(value.username);
+        allUsers.push(value.username);
+      }
+
+      if (allUsers.length > 0) {
+        io.to(socket.id).emit('listUsers', allUsers);
+      }
+    });
+
+    socket.on('listRoomUsers', (room) => {
+      let socketIds;
+      let members = [];
+
+      for (let [key, value] of io.of('/').adapter.rooms) {
+        if (key.toLowerCase() === room) {
+          socketIds = value;
+        }
+      }
+
+      console.log("socket id's", socketIds);
+
+      if (socketIds) {
+        socketIds.forEach((socketId) => {
+          let socketInRoom = io.of('/').sockets.get(socketId);
+          members.push(socketInRoom.username);
+        });
+        console.log('room members', members);
+
+        io.to(socket.id).emit('listRoomUsers', { members, room });
+      }
+    });
   });
 };
