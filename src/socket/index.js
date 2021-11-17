fefb89a1a74ad02295a4a05ab22c2c3ce8a6caba
kason <kbraley9@gmail.com>
2021-11-16 17:25:35 -0700

    Fix username display issues

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,19 +2,19 @@ module.exports = function (io) {
   io.on('connection', (socket) => {
     console.log(`Socket ${socket.id} connected`);
 
-    socket.on('message', ({ user, message, room }) => {
-      io.to(room).emit('message', { user, message });
+    socket.on('message', ({ message, room }) => {
+      io.to(room).emit('message', { username: socket.username, message });
     });
 
     socket.on('join', ({ room, username }) => {
       console.log(`${socket.id} just joined room [${room}]`);
       socket.join(room);
       socket.username = username;
-      io.to(room).emit('user-connected', socket.id);
+      io.to(room).emit('user-connected', username);
     });
 
-    socket.on('quit', (payload) => {
-      socket.broadcast.emit('quit', socket.id);
+    socket.on('quit', () => {
+      socket.broadcast.emit('quit', socket.username);
       socket.disconnect(true);
     });
 
