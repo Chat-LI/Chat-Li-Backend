d1b54bd8e69c4b100b18ee53722d17a1bf2d0a4c
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-11-18 16:57:37 -0800

    fix unit tests for routes

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,91 +1,109 @@
 'use strict';
 
-// const supertest = require('');
-// const app = require('');
-// const server = require('');
-// const jwt = require('');
-
-// const start = supertest;
 const supertest = require('supertest');
 const app = require('../src/server.js');
 const server = supertest(app.server);
-const jwt = require('jsonwebtoken');
-
 const { db } = require('../src/models');
-
 beforeAll(async () => {
   await db.sync();
-  // done();
 });
-
 afterAll(async () => {
   await db.drop();
-  // done();
 });
-
 describe('Checks if Auth routes are properly functioning', () => {
   let userInfo = {
     admin: { username: 'admin123', password: 'password', role: 'admin' },
     user: { username: 'user123', password: 'password', role: 'user' },
   };
-
   let roomInfo = {
     roomname: 'room1',
     password: 'password1',
   };
-
   it('can create a new user and sends an object with the user and the token to the client', async () => {
     const response = await server.post('/signup').send(userInfo['user']);
     const userObject = response.body;
-
-    //expect(response.status).toBe(201);
     expect(userObject.token).toBeDefined();
     expect(userObject.user.id).toBeDefined();
     expect(userObject.user.username).toEqual(userInfo['user'].username);
-    // done();
+  });
+  it('can join a room', async () => {
+    await server.post('/rooms').send(roomInfo);
+    const response = await server
+      .post('/joinRoom')
+      .send({ isRoom: true })
+      .auth(roomInfo.roomname, roomInfo.password);
+    const roomObject = response.body;
+    expect(roomObject.room.roomname).toEqual(roomInfo.roomname);
+    expect(roomObject.room.password).toBeDefined();
   });
 });
+describe('Checks if the user routes are properly functioning', () => {
+  let userInfo = {
+    admin: { username: 'admin123', password: 'password', role: 'admin' },
+    user: { username: 'user123', password: 'password', role: 'user' },
+  };
+  it('can allow admins to get one user, not allow users to do so', async () => {
+    const response = await server.post('/signup').send(userInfo.admin);
+    let user = { username: 'user1234', password: 'password', role: 'user' };
+    const responseUser = await server.post('/signup').send(user);
+    const token = response.body.user.token;
+    const userToken = responseUser.body.user.token;
+    const response2 = await server
+      .get('/users/1')
+      .set('Authorization', Bearer`${token}`);
+    const responseUser2 = await server
+      .get('/users/1')
+      .set('Authorization', Bearer`${userToken}`);
+    expect(response2.body.username).toBe('user123');
+    expect(responseUser2.status).toBe(500);
+  });
+  it('can allow admins to get all users, not allow users to do so', async () => {
+    let admin = { username: 'admin1234', password: 'password', role: 'admin' };
+    const response = await server.post('/signup').send(admin);
+    let user = { username: 'user12345', password: 'password', role: 'user' };
+    const responseUser = await server.post('/signup').send(user);
+    const token = response.body.user.token;
+    const userToken = responseUser.body.user.token;
+    const response2 = await server
+      .get('/users')
+      .set('Authorization', Bearer`${token}`);
+    const responseUser2 = await server
+      .get('/users')
+      .set('Authorization', Bearer`${userToken}`);
+    expect(response2.body.length).toBe(5);
+    expect(responseUser2.status).toBe(500);
+  });
+  it('can allow admins to delete users, not allow users to do so', async () => {
+    let admin = { username: 'admin12345', password: 'password', role: 'admin' };
+    const response = await server.post('/signup').send(admin);
+    let user = { username: 'user123456', password: 'password', role: 'user' };
+    const responseUser = await server.post('/signup').send(user);
+    const token = response.body.user.token;
+    const userToken = responseUser.body.user.token;
+    await server.delete('/users/1').set('Authorization', Bearer`${token}`);
+    const responseUser2 = await server
+      .delete('/users/2')
+      .set('Authorization', Bearer`${userToken}`);
+    const getCheck = await server
+      .get('/users/1')
+      .set('Authorization', Bearer`${token}`);
+    expect(getCheck.body).toBeNull();
+    expect(responseUser2.status).toBe(500);
+  });
+  it('can allow admins to create users, not allow users to do so', async () => {
+    let admin = { username: 'admin12345', password: 'password', role: 'admin' };
+    const response = await server.post('/signup').send(admin);
+    let user = { username: 'user123456', password: 'password', role: 'user' };
+    const responseUser = await server.post('/signup').send(user);
+    const token = response.body.user.token;
+    const userToken = responseUser.body.user.token;
+    const response2 = await server
+      .post('/users')
+      .set('Authorization', Bearer`${token}`)
+      .set('Content-Type', 'application/json')
+      .send({ username: 'foo', password: 'bar', role: 'user' });
 
-// it('can log in a user and sends an object with the user and token to the client', async (done) => {
-//   const response = await server
-//     .post('/signin')
-//     .auth(userInfo['user'].username, userInfo['user'].password);
-
-//   const userObject = response.body;
-//   expect(response.status).toBe(200);
-//   // expect(userObject.token).toBeDefined();
-//   expect(userObject.user.id).toBeDefined();
-//   expect(userObject.user.username).toEqual(userInfo['user'].username);
-//   done();
-// });
-
-// it('can join a room', async () => {
-//   await server.post('/rooms').send(roomInfo);
-//   const response = await server
-//     .post('/joinRoom')
-//     // .send(roomInfo)
-//     .auth(roomInfo.roomname, roomInfo.password);
-//   const roomObject = response;
-//   console.log(response);
-
-//   expect(roomObject.roomname).toEqual(roomInfo.roomname);
-//   expect(roomObject.password).toEqual(roomInfo.password);
-
-// done();
-//   });
-// });
-
-// describe('Checks if the user routes are properly functioning', () => {
-//   let userInfo = {
-//     admin: { username: 'admin123', password: 'password', role: 'admin' },
-//     user: { username: 'user123', password: 'password', role: 'user' },
-//   };
-
-//   it('can allow admins to get one user, not allow users to do so', () => {});
-
-//   it('can allow admins to get all users, not allow users to do so', () => {});
-
-//   it('can allow admins to create users, not allow users to do so', () => {});
-
-//   it('can allow admins to delete users, not allow users to do so', () => {});
-// });
+    expect(response2.body.user.username).toBe('foo');
+    expect(responseUser2.status).toBe(500);
+  });
+});
